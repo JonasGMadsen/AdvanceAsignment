10c7ee2a6616a6d88a0b8a7417baa003e2cbfced
JonasGMadsen <Jonas6070@hotmail.com>
2024-04-14 00:17:29 +0200

    Tracing and logging made + interface. Need to implement

---
+++ TracingAndLogging/ITracingAndLogging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceAsignment.TracingAndLogging
+{
+    internal interface ITracingAndLogging
+    {
+        void WriteInfoToText(string information);
+        void WriteWarningToText(string warning);
+        void WriteErrorToText(string error);
+    }
+}
--- TracingAndLogging/TracingAndLogging.cs
+++ TracingAndLogging/TracingAndLogging.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdvanceAsignment.TracingAndLogging
 {
-    public class TracingAndLogging
+    public class TracingAndLogging : ITracingAndLogging
     {
-        //TODO
+        private TraceSource traceSource = new TraceSource("AdvanceAsignment");
+        private TextWriterTraceListener textWriter;
+
+        private static readonly Lazy<TracingAndLogging> instance = new Lazy<TracingAndLogging>(() => new TracingAndLogging()); //Lazy initialization. For a sikre thread safety. Experimentielt. 
+
+        public static TracingAndLogging Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        private TracingAndLogging()
+        {
+            traceSource.Switch = new SourceSwitch("TracingAndLogging", "All");
+            textWriter = new TextWriterTraceListener(new StreamWriter("log.txt") { AutoFlush = true });
+            traceSource.Listeners.Add(textWriter);
+        }
+
+        public void WriteErrorToText(string error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteInfoToText(string writeToTxt)
+        {
+            traceSource.TraceEvent(TraceEventType.Information, 200, writeToTxt);
+        }
+
+        public void WriteWarningToText(string warning)
+        {
+            traceSource.TraceEvent(TraceEventType.Warning, 100, warning);
+        }
+
     }
 }
